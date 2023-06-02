04949142d5b1764cb9385fdb8aa2219eeba0af5a
JoseAntonioBretonesGarcia <joseantoniobregar@gmail.com>
2023-06-02 13:27:18 +0200

    Section 11, class 156.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+
+//Configuración del locale de la app
+import localeEsHN from '@angular/common/locales/es-HN';
+import localeFrCA from '@angular/common/locales/fr-CA';
+
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEsHN);
+registerLocaleData(localeFrCA);
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,7 +23,12 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
     BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-HN',
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
